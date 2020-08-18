1cc8c24f704607a151eb440ad7fc1be24531903c
Mihail Kirov <mihail.kirov2@gmail.com>
2020-08-18 16:12:15 +0300

    Migrating fileshare app into ecommerce clothing app

---
+++ src/main/java/com/example/drippyshop/DrippyshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.drippyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrippyshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrippyshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/drippyshop/DrippyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drippyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrippyshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
